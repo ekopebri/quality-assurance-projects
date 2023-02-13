071f295257f991417d9960675dd9e6fa40a4ab91
ekopebri <eko.pebrisulistiyo@gmail.com>
2023-02-13 09:08:35 +0700

    feat: finish learn advancednode

--- learn-advancednode/routes.js
+++ learn-advancednode/routes.js
@@ -1,7 +1,7 @@
-const passport = require("passport");
-const bcrypt = require("bcrypt");
+const passport = require('passport');
+const bcrypt = require('bcrypt');
 
-module.exports = function (app, myDatabase) {
+module.exports = function (app, myDataBase) {
     app.route('/').get((req, res) => {
         res.render('index', {
             title: 'Connected to Database',
@@ -12,65 +12,71 @@ module.exports = function (app, myDatabase) {
         });
     });
 
-    app.route("/login")
-        .post(passport.authenticate('local', {failureRedirect: '/'}), (req, res) => {
-            res.redirect('/profile');
-        });
+    app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+        res.redirect('/profile');
+    });
 
-    app.route('/profile')
-        .get(ensureAuthenticated, (req, res) => {
-            res.render('profile', {
-                username: req.user.username
-            });
-        });
+    app.route('/profile').get(ensureAuthenticated, (req,res) => {
+        res.render('profile', { username: req.user.username });
+    });
 
-    app.route("/logout")
-        .get((req, res) => {
-            req.logout();
-            res.redirect('/')
-        });
+    app.route('/chat').get(ensureAuthenticated, (req, res) => {
+        res.render('chat', { user: req.user });
+    });
 
-    app.route("/register")
-        .post((req, res, next) => {
-            myDatabase.findOne({username: req.body.username}, (err, user) => {
+    app.route('/logout').get((req, res) => {
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.route('/register').post((req, res, next) => {
+            const hash = bcrypt.hashSync(req.body.password, 12);
+            myDataBase.findOne({ username: req.body.username }, (err, user) => {
                 if (err) {
                     next(err);
                 } else if (user) {
                     res.redirect('/');
                 } else {
-                    // Hash password before save to database
-                    const hash = bcrypt.hashSync(req.body.password, 12);
-
-                    myDatabase.insertOne({
-                        username: req.body.username,
-                        password: hash
-                    }, (err, doc) => {
-                        if (err) {
-                            res.redirect('/');
-                        } else {
-                            next(null, doc.ops[0]);
+                    myDataBase.insertOne({
+                            username: req.body.username,
+                            password: hash
+                        },
+                        (err, doc) => {
+                            if (err) {
+                                res.redirect('/');
+                            } else {
+                                // The inserted document is held within
+                                // the ops property of the doc
+                                next(null, doc.ops[0]);
+                            }
                         }
-                    })
+                    )
                 }
             })
-        }, passport.authenticate('local', { failureRedirect: '/' }), (req, res, next) => {
+        },
+        passport.authenticate('local', { failureRedirect: '/' }),
+        (req, res, next) => {
             res.redirect('/profile');
-        });
+        }
+    );
 
-    app.route("/auth/github")
-        .get(passport.authenticate('github'));
+    app.route('/auth/github').get(passport.authenticate('github'));
+    app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+        console.log("session: ",res.session);
+        res.session.user_id = req.user.id;
+        res.redirect("/chat");
+    })
 
-    app.route("/auth/github/callback")
-        .get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
-            res.redirect('/profile');
-        });
+    app.use((req, res, next) => {
+        res.status(404)
+            .type('text')
+            .send('Not Found');
+    });
 }
 
-// Create middleware for ensure if user is authenticated
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
-
     res.redirect('/');
-}
+};
